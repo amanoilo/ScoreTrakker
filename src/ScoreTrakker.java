43c17ea91aa50efba563c9593687afe3409cbd9f
Alex Manoilo <amanoilo@mymail.mines.edu>
2015-02-09 23:17:53 -0700

    Finished Part III.

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,3 +1,12 @@
+/*EXCEPTIONS HAIKU
+ * 
+ * Things aren't going right?
+ * You should change your perception:
+ * Make an exception.
+ * 
+ */
+
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -7,29 +16,36 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
-	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
-	void loadDataFromFile(String filename) throws FileNotFoundException
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt", "badname.txt"};
+	void loadDataFromFile(String filename) throws Exception
 	{
 		students = new ArrayList<Student>();
 		FileReader reader = new FileReader(filename);
 		Scanner scan = new Scanner(reader);
 		while (scan.hasNextLine())
 		{
-			String names = scan.nextLine();
-			String points = scan.nextLine();
-			try
-			{
-			int pointsInt = Integer.parseInt(points);
-			students.add(new Student(names, pointsInt));
-
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("\"" + points + "\" is not a valid score for " + names+".");
-				System.out.println();
-			}
-	
 			
+
+				String names = scan.nextLine();
+			if(names.indexOf(" ") != -1)
+			{
+				String points = scan.nextLine();
+				try
+				{
+					int pointsInt = Integer.parseInt(points);
+					students.add(new Student(names, pointsInt));
+
+				}
+				catch (NumberFormatException e)
+				{
+					System.out.println("\"" + points + "\" is not a valid score for " + names+".");
+					System.out.println();
+				}		
+			}
+			else 
+			{
+				throw new Exception("\"" + names + "\" is missing either a first or last name.");
+			}
 		}
 	}
 	
@@ -56,8 +72,13 @@ public class ScoreTrakker {
 			{
 				System.out.println(i + " does not exist in the current directory.");
 			}
+			catch (Exception f)
+			{
+				System.out.println(f.getMessage());
+			}
 		}
 	}
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		ScoreTrakker keeper = new ScoreTrakker();
