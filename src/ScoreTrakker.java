5a6515656e00586038043e75372f9a38dcaea4e4
Alex Manoilo <amanoilo@mymail.mines.edu>
2015-02-09 22:51:18 -0700

    Did Part II: Handling Exceptions

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
-	
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
 	void loadDataFromFile(String filename) throws FileNotFoundException
 	{
 		students = new ArrayList<Student>();
@@ -17,8 +17,18 @@ public class ScoreTrakker {
 		{
 			String names = scan.nextLine();
 			String points = scan.nextLine();
+			try
+			{
 			int pointsInt = Integer.parseInt(points);
 			students.add(new Student(names, pointsInt));
+
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("\"" + points + "\" is not a valid score for " + names+".");
+				System.out.println();
+			}
+	
 			
 		}
 	}
@@ -26,16 +36,27 @@ public class ScoreTrakker {
 	void printInOrder()
 	{
 		Collections.sort(students);
+		System.out.println("Students and Scores");
 		for(Student i : students)
 		{
 			System.out.println(i.toString());
 		}
+		System.out.println();
 	}
 	
-	void processFiles() throws Exception
+	void processFiles()
 	{
-		loadDataFromFile("scores.txt");
-		printInOrder();
+		for (String i : files)
+		{
+			try{
+				loadDataFromFile(i);
+				printInOrder();
+			}
+			catch (FileNotFoundException e)
+			{
+				System.out.println(i + " does not exist in the current directory.");
+			}
+		}
 	}
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
